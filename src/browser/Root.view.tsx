de08ef9f2dd87a855c532ec7aebdb5a1917b96e2
Zero <1203970284@qq.com>
2024-02-06 17:12:10 +0800

    feat: add commitlint, husky, link staged

--- src/browser/Root.view.tsx
+++ src/browser/Root.view.tsx
@@ -1,15 +1,18 @@
 import { Component, TSX, Vue } from "vue-facing-decorator";
+import Logo from "./images/logo.png";
 
 @Component({
   render() {
     return (
-      <div class="h-screen w-screen flex flex-col justify-center items-center text-center">
+      <div class="h-screen w-screen flex flex-col justify-center items-center text-center gap2">
+        <img src={Logo} />
+        <hr />
         <h1>Malagu Rpc + Vue 3 + Vue Router + Pinia + Vue Decorator + TSX</h1>
         <h2>UNOCSS + Less</h2>
-        <h3>ESLint + Prettier + </h3>
+        <h3>ESLint + Prettier + CommitLint + Husky + Link Staged</h3>
         <hr />
         <p>
-          Powered by <a href="">Zero</a>
+          Powered by <a href="https://github.com/Groupguanfang">Zero</a>
         </p>
         <p>Notice: This template support bun, but malagu isn't support pnpm, so recommend use bun in project.</p>
       </div>
